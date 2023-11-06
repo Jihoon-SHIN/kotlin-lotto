316e871ffa3b39b249277650dbfac863858a6523
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-07 01:17:21 +0900

    chore : LottoChecker 의 function 순서변경

--- src/main/kotlin/lotto/domain/LottoChecker.kt
+++ src/main/kotlin/lotto/domain/LottoChecker.kt
@@ -3,21 +3,21 @@ package lotto.domain
 import lotto.domain.dto.LotteryWinningStatistics
 
 class LottoChecker() {
+    fun getWinNumStatistics(lottos: Lottos, winningNumbers: List<Int>, bonusBalls: Int = NO_BONUS_BALL): LotteryWinningStatistics {
+        val statistics = mutableMapOf<LotteryPrizeAmount, Int>()
+        val lottoList = lottos.lottoList
+        lottoList.forEach { lotto: Lotto ->
+            val lotteryPrize = getLotteryPrize(lotto, winningNumbers, bonusBalls)
+            statistics[lotteryPrize] = statistics.getOrDefault(lotteryPrize, 0) + 1
+        }
+        return LotteryWinningStatistics(statistics)
+    }
+
     fun getLotteryPrize(lotto: Lotto, winningNumbers: List<Int>, bonusBalls: Int = NO_BONUS_BALL): LotteryPrizeAmount {
         val lottoNums = lotto.numbers
         val winCount = lottoNums.intersect(winningNumbers).size
         val isContainBonus = lottoNums.contains(bonusBalls)
         return LotteryPrizeAmount.getWinningPrize(winCount, isContainBonus)
-    }
-
-    fun getWinNumStatistics(lottos: Lottos, winningNumbers: List<Int>): LotteryWinningStatistics {
-        val statistics = mutableMapOf<LotteryPrizeAmount, Int>()
-        val lottoList = lottos.lottoList
-        lottoList.forEach { lotto: Lotto ->
-            val lotteryPrize = getLotteryPrize(lotto, winningNumbers)
-            statistics[lotteryPrize] = statistics.getOrDefault(lotteryPrize, 0) + 1
-        }
-        return LotteryWinningStatistics(statistics)
     }
 
     companion object {
