f19a30fcc3d43d1f6b9ccc0a300d9c172bf84f26
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-07 00:30:56 +0900

    fix : 2등 추가를 위해 LotteryPrizeAmount 를 변경합니다

--- src/main/kotlin/lotto/domain/LotteryPrizeAmount.kt
+++ src/main/kotlin/lotto/domain/LotteryPrizeAmount.kt
@@ -1,13 +1,14 @@
 package lotto.domain
 
 enum class LotteryPrizeAmount(val winNum: Int, val prize: Int) {
-    ZERO_WIN(0, 0),
-    ONE_WIN(1, 0),
-    TWO_WIN(2, 0),
-    THREE_WIN(3, 5000),
-    FOUR_WIN(4, 50000),
-    FIVE_WIN(5, 1500000),
-    SIX_WIN(6, 2000000000),
+    MISS0(0, 0),
+    MISS1(1, 0),
+    MISS2(2, 0),
+    FIFTH(3, 5000),
+    FOURTH(4, 5000),
+    THIRD(5, 50000),
+    SECOND(5, 1500000),
+    FIRST(6, 2000000000),
     ;
 
     companion object {
